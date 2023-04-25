da5d11ed637f4177594062a0e15e97adc0286a98
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-04-25 16:59:01 +0600

    Redis Introduce

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Button from '~/components/ui/Button'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default async function Home() {
   return (
     <Button>
       Click Me
