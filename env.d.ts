f26e268c7d3ac09b32d83620e228fada9fdc9439
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-08-03 21:52:26 +0600

    ➖ reomoved everything

--- env.d.ts
+++
@@ -1,6 +0,0 @@
-namespace NodeJS {
-  interface ProcessEnv {
-    UPSTASH_REDIS_REST_URL: string;
-    UPSTASH_REDIS_REST_TOKEN: string;
-  }
-}
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Button from '~/components/ui/Button'
-
-export default async function Home() {
-  return (
-    <Button>
-      Click Me
-    </Button>
-  )
-}
